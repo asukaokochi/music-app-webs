c4686afaf729f20cfb1111c8f6d7455dc29ef725
asukaokochi <x17018xx@gmail.com>
2019-02-23 21:24:33 +0900

    [add]それっぽくlogin前を作った

--- app.rb
+++ app.rb
@@ -4,3 +4,40 @@ require 'sinatra/reloader' if development?
 
 require 'sinatra/activerecord'
 require './models'
+
+enable :sessions
+
+get '/' do
+  erb :index
+end
+
+get '/signup' do
+  erb :sign_up
+end
+
+post '/signup' do
+  user = User.create(
+    name: params[:user_name],
+    password: params[:password],
+    password_confirmation: params[:password_confirmation]
+  )
+  if user.persisted?
+    session[:user] = user.id
+  end
+  redirect '/'
+end
+
+get '/signin' do
+  erb :sign_in
+end
+
+get '/signout' do
+  session[:user] = nil
+  redirect '/'
+end
+
+helpers do
+  def current_user
+    User.find_by(id: session[:user])
+  end
+end
